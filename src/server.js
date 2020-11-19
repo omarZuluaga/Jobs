5cb4510985714e010f92291e270e6a27182f41c9
omarZuluaga <omarzuluaga123@hotmail.com>
2020-11-19 03:24:45 -0500

    correccion de errores en despliegue

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/sistema-geolocalizacion-tareas-de-campo'));
+app.use(express.static(__dirname+'../dist/sistema-geolocalizacion-tareas-de-campo'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/sistema-geolocalizacion-tareas-de-campo/index.html'));
+    res.sendFile(path.join(__dirname+'../dist/sistema-geolocalizacion-tareas-de-campo/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
