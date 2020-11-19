a6e680202acf13e3d6c32e5c05f39bcbacb6ae9f
omarZuluaga <omarzuluaga123@hotmail.com>
2020-11-19 03:44:28 -0500

    corrección de error en path

--- src/server.js
+++ src/server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(express.static(__dirname+'../dist/sistema-geolocalizacion-tareas-de-campo'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'../dist/sistema-geolocalizacion-tareas-de-campo/index.html'));
+    res.sendFile(path.join(__dirname+'./dist/sistema-geolocalizacion-tareas-de-campo/index.html'));
+    console.log(__dirname+'./dist/sistema-geolocalizacion-tareas-de-campo/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
